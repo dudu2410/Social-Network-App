2ef633e0138fd3d641bdc9d3d00b0f5192369cfb
dudu2410 <ngddu2410@gmail.com>
2018-11-30 22:23:23 +0700

    install redux, route

---
+++ social-network-app/src/Components/Header.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import '../Css/Header.css'
+
+export default class Header extends Component {
+    render() {
+        return(
+            <div> ABC </div>
+        )
+    }
+}
--- social-network-app/src/index.js
+++ social-network-app/src/index.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+const Header = lazy(() => import('./Components/Header.js'));
+
+
+const App = () => (
+  <Router>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Switch>
+        <Route exact path="/" component={Header}/>
+        <Route path="/home" component={Header}/>
+      </Switch>
+    </Suspense>
+  </Router>
+);
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
