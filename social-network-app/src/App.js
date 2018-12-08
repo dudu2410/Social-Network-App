83bcf7a012dba4526807de2df1ae4e2c86864a93
dudu2410 <ngddu2410@gmail.com>
2018-12-08 15:09:18 +0700

    fix nhẹ giao diện

--- social-network-app/src/App.js
+++ social-network-app/src/App.js
@@ -2,11 +2,12 @@ import React, { Component } from 'react';
 import AppNavBar from './Containers/HeaderContainer';
 import PostListContainer from './Containers/PostListContainer';
 import { Wall } from './Components/Wall';
+import './Css/App.css'
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className="background">
         <AppNavBar />
         <Wall></Wall>
         <div className="container">
--- social-network-app/src/Components/PostList.js
+++ social-network-app/src/Components/PostList.js
@@ -5,7 +5,7 @@ import { Post } from '../Components/Post'
 export const PostList = ({ posts }) => {
     console.log(posts);
     return (
-        <ul>
+        <ul style={{background: '#FFFFFF'}}>
             {posts.map((post, index) => (
                 <Post key={post.id} id={post.id} avatar={post.avatar} username={post.username}
                     content={post.content} comment={post.comment} share={post.share}
