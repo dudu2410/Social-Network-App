de77db81c9262492aac14e28ba7d8406068e4293
NGUYEN MINH DONG <1512117@student.hcmus.edu.vn>
2018-12-18 00:58:24 +0700

    add AddFollow action

--- social-network-app/src/Actions/Actions.js
+++ social-network-app/src/Actions/Actions.js
@@ -1,6 +1,7 @@
 
 
 export const ADD_POST = 'ADD_POST';
+export const ADD_FOLLOW = 'ADD_FOLLOW';
 
 
 export function addPost(id, content, timestamp, avatar, username, heart, comment, share) {
@@ -14,5 +15,16 @@ export function addPost(id, content, timestamp, avatar, username, heart, comment
         heart,
         comment,
         share
+    }
+}
+
+export function addFollow(id1,username1,id2,username2)
+{
+    return{
+        type: ADD_FOLLOW,
+        id1,
+        username1,
+        id2,
+        username2
     }
 }
