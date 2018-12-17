a4efdd2b29f11e2d15d03fb039ce83f9ea502838
dudu2410 <ngddu2410@gmail.com>
2018-12-17 18:35:08 +0700

    json respone

--- Backend/service.js
+++ Backend/service.js
@@ -5,7 +5,7 @@ server.listen(3001);
 
 app.get("/", function(req,res){
     res.setHeader('Content-Type', 'application/json');
-    res.send(JSON.stringify({ a: 1 }));
+    res.send(JSON.stringify({ a: 1 }, null, 3));
 });
 
 app.get("/vidu", function(req,res){
