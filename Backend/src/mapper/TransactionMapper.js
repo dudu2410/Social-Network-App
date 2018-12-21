6959d93889126391d6e080c74aea75fe118e26a9
huudanh2808 <huudanh2808@gmail.com>
2018-12-21 23:35:28 +0700

    [ADD] add followings to return data of get user info service

--- Backend/src/mapper/TransactionMapper.js
+++ Backend/src/mapper/TransactionMapper.js
@@ -1,5 +1,8 @@
-var { decode, encode, sign, encodePostContent, decodePostContent, hash, decodeFollowings } = require("../../lib/tx");
-
+var { decode, encode,
+    sign, encodePostContent,
+    decodePostContent, hash,
+    encodeBase32,
+    decodeFollowings } = require("../../lib/tx");
 const POST_OPERATION = 'post';
 const PAYMENT_OPERATION = 'payment';
 const CREATE_ACCOUNT_OPERATION = 'create_account';
@@ -44,10 +47,31 @@ var toSimpleTransactionInfo = (tx) => {
             break;
         }
         case UPDATE_ACCOUNT_OPERATION: {
+            var followings_address = {};
+            if (txjson.params.key === 'followings') {
+                var decodedAddress = decodeFollowings(txjson.params.value);
+                var addresses = [];
+                decodedAddress.addresses.forEach((address) => {
+                    if (address instanceof Buffer) {
+                        try {
+                            var afterEncodeBase32Address = encodeBase32(address);
+                            if (typeof afterEncodeBase32Address === 'string') {
+                                addresses.push(afterEncodeBase32Address);
+                            }
+                        }
+                        catch (err) {
+                            console.log(err);
+                        }
+                    }
+                })
+                followings_address = {
+                    addresses: addresses
+                }
+            }
             simpleTransactionInfo.type = UPDATE_ACCOUNT_OPERATION;
             simpleTransactionInfo.content_type = txjson.params.key;
             simpleTransactionInfo.content = txjson.params.key === 'name' ? txjson.params.value.toString('utf-8') :
-                txjson.params.key === 'followings' ? decodeFollowings(txjson.params.value) :
+                txjson.params.key === 'followings' ? followings_address :
                     txjson.params.value.toString('base64');
             simpleTransactionInfo.from = txjson.account;
             simpleTransactionInfo.sequence = txjson.sequence;
@@ -64,4 +88,4 @@ var toSimpleTransactionInfo = (tx) => {
 
 }
 
-module.exports = { toSimpleTransactionInfo ,UPDATE_ACCOUNT_OPERATION };
+module.exports = { toSimpleTransactionInfo, UPDATE_ACCOUNT_OPERATION };
