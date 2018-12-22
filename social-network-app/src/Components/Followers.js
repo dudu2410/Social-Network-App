c8829da6e78f4afc5a66074d820b0dc5aa482bac
Nguyễn Minh Đồng (Chris Nguyen) <35996885+nmdong140797github@users.noreply.github.com>
2018-12-22 23:25:54 +0700

    simple link for followed people

--- social-network-app/src/Components/Followers.js
+++ social-network-app/src/Components/Followers.js
@@ -8,7 +8,12 @@ const Followers = ({followers}) => {
     <div className="background">
     <ul className="list-group">
     {followers.map((follower) => (
-      <li className="list-group-item" key = {follower.id}>{follower.username}</li>
+      <li className="list-group-item" key = {follower.id}><a href="/" style = {{color : "red"}}>
+      <img src='https://scontent.fsgn2-3.fna.fbcdn.net/v/t1.0-9/20883051_1838227159796757_3354415150688791402_n.jpg?_nc_cat=108&_nc_ht=scontent.fsgn2-3.fna&oh=bbb0cf33a38f7482fb6550d5d315191f&oe=5C9E33F4' 
+      className="img-circle" alt={follower.username} style={{width:'20%'}} ></img>
+      {follower.username}
+      </a>
+      </li>
                     ))}
             </ul>
         </div>
