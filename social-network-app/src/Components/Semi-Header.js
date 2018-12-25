2cec6ebc053c8504d45adb13ba8bef1f8651662c
dudu2410 <ngddu2410@gmail.com>
2018-12-25 21:06:00 +0700

    CHuyển thêm Link vào Semi-Header.js

--- social-network-app/src/Components/Semi-Header.js
+++ social-network-app/src/Components/Semi-Header.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import '../Css/Semi-Header.css'
+import {Link} from 'react-router-dom'
 
 export const SemiHeader = ({followers,onFollowClick}) => {
     console.log("ham la:" + onFollowClick);
@@ -12,14 +13,13 @@ console.log("oject: "+owner);
     return (
         
         <ul>
-            <li className="space"><a href="/" style = {{color : "red"}}>Posts</a></li>
-            <li><a href="/followers" style = {{color : "red"}}>Followers</a></li> 
-            <li>
-                <a href="/followers" style = {{color : "red"}} onClick={(e) => {onFollowClick(e, owner);}}>Follow</a>
-            </li>
-            <li><a href="/" style = {{color : "black"}}>Likes</a></li>
-            <li><a href="/" style = {{color : "black"}}>Lists</a></li>
-            <li><a href="/" style = {{color : "black"}}>Moments</a></li>
+            <Link to="/"><li className="space"><a href='' style = {{color : "red"}}>Posts</a></li></Link>
+            <Link to="/followers"><li><a href='' style = {{color : "red"}}>Followers</a></li></Link> 
+            <Link to="/"><li><a href="/followers" style = {{color : "red"}} onClick={(e) => {onFollowClick(e, owner);}}>Follow</a>
+            </li></Link>
+            <Link to="/"><li><a href='' style = {{color : "black"}}>Likes</a></li></Link>
+            <Link to="/"><li><a href='' style = {{color : "black"}}>Lists</a></li></Link>
+            <Link to="/"><li><a href='' style = {{color : "black"}}>Moments</a></li></Link>
         </ul>
         
     )
